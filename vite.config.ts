ce9fcef951135ef10f8a67c58fe7af5e80389e28
Greg Duckworth <gregory.duckworth@nearform.com>
2023-10-20 15:24:12 +0100

    fix: use hashrouter instead browserrouter

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/testing-playground',
+  base: process.env.NODE_ENV === 'production' ? '/#/testing-playground/' : '/#/',
   build: {
     minify: false
   },
