1bd5af88fdc1d3cfd99af9deb07574674beec6f8
Greg Duckworth <gregory.duckworth@nearform.com>
2024-02-02 11:45:00 +0000

    feat: update to new Nearform branding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import GitHubIcon from '@mui/icons-material/GitHub'
-import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Toolbar, Typography, useTheme } from '@mui/material'
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
@@ -8,20 +8,34 @@ import LanguageDropdown from './LanguageDropdown'
 
 function Header (): JSX.Element {
   const { t } = useTranslation()
+  const theme = useTheme()
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+      <AppBar
+        position='static'
+        sx={{ backgroundColor: theme.palette.secondary.main }}
+      >
         <Toolbar sx={{ justifyContent: 'center', alignItems: 'center' }}>
-          <Typography variant='h6'>
-            <Link to='/' style={{ color: 'inherit', textDecoration: 'none' }}>
+          <img src='nearform_logo_white.svg' alt='Nearform' />
+          <Typography variant='h6' sx={{ flexGrow: 1, textAlign: 'center' }}>
+            <Link
+              to='/'
+              style={{
+                color: theme.palette.secondary.contrastText,
+                textDecoration: 'none'
+              }}
+            >
               {t('common.title')}
             </Link>
           </Typography>
-          <Box sx={{ flexGrow: 1 }} />
           <Link
             to='https://github.com/nearform/testing-playground'
             target='_blank'
-            style={{ color: 'inherit', marginRight: 20 }}
+            style={{
+              color: theme.palette.secondary.contrastText,
+              marginRight: 20
+            }}
           >
             <GitHubIcon />
           </Link>
